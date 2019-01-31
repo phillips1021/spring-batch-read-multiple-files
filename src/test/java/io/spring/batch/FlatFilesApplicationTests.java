081826a75c3deecda8c732ade49d066d6393e18f
bphillips <bphillips@cboe.com>
2019-01-31 16:30:33 -0600

    initial commit

---
+++ src/test/java/io/spring/batch/FlatFilesApplicationTests.java
@@ -0,0 +1,16 @@
+package io.spring.batch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlatFilesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
